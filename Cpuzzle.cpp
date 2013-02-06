e8c84f45eacd9e7f0e871f61bfac6ee36b09deac
jhm9318 <jhm9318@gmail.com>
2013-02-06 12:48:00 +0330

    change basic functions

--- Cpuzzle.cpp
+++ Cpuzzle.cpp
@@ -5,21 +5,8 @@
 #include <ctime>
 
 Cpuzzle::Cpuzzle()
-{
-
-    for(int i=0;i<3;i++)
-        elements_3[i] = "###";
-    
-    for(int i=0;i<4;i++)
-        elements_4[i] = "####";
-
-    for(int i=0;i<5;i++)
-        elements_5[i] = "#####";
-        
-    for(int i=0;i<6;i++)
-        elements_6[i] = "######";    
-    
-    for(int i=0;i<7;i++)
+{    
+    for(int i=0;i<15;i++)
         nol[i]=0;
     
     word_2.clear();    
@@ -27,37 +14,28 @@ Cpuzzle::Cpuzzle()
     word_4.clear();
     word_5.clear();
     word_6.clear();
+    word_7.clear();
+    word_8.clear();
+    word_9.clear();
+    word_10.clear();
+    word_11.clear();
+    word_12.clear();
+    word_13.clear();
+    word_14.clear();
+    word_15.clear();    
         
     initialize();
     srand(time(0));
     
-    if(rand()%2)
-        def_puzzle_1();
-    else
-        def_puzzle_2();
+    clear();
+    def_puzzle();
+    
 }
 
-void Cpuzzle::clear(int size)
+void Cpuzzle::clear()
 {
-    switch(size)
-    {
-        case 3: 
-            for(int i=0;i<3;i++)
-                elements_3[i] = "###";
-            break;
-        case 4: 
-            for(int i=0;i<4;i++)
-                elements_4[i] = "####";
-            break;
-        case 5: 
-            for(int i=0;i<5;i++)
-                elements_5[i] = "#####";
-            break;
-        case 6: 
-            for(int i=0;i<6;i++)
-                elements_6[i] = "######";
-            break;
-    }
+    for(int i=0;i<15;i++)
+        elements[i] = "###############";
 }
 
 void Cpuzzle::initialize()
@@ -65,8 +43,7 @@ void Cpuzzle::initialize()
     ifstream file;
     string temp;
         
-    file.open("words/2.txt");    
-        
+    file.open("words/2.txt");            
     while( getline(file, temp) )
     {
         word_2.push_back(temp);
@@ -74,8 +51,7 @@ void Cpuzzle::initialize()
     }
     file.close();
     
-    file.open("words/3.txt");    
-    
+    file.open("words/3.txt");        
     while( getline(file, temp) )
     {
         word_3.push_back(temp);
@@ -83,8 +59,7 @@ void Cpuzzle::initialize()
     }
     file.close();
     
-    file.open("words/4.txt");    
-    
+    file.open("words/4.txt");        
     while( getline(file, temp) )
     {
         word_4.push_back(temp);
@@ -92,8 +67,7 @@ void Cpuzzle::initialize()
     }
     file.close();
     
-    file.open("words/5.txt");    
-    
+    file.open("words/5.txt");        
     while( getline(file, temp) )
     {        
         word_5.push_back(temp);
@@ -101,92 +75,98 @@ void Cpuzzle::initialize()
     }
     file.close();
     
-    file.open("words/6.txt");    
-    
+    file.open("words/6.txt");        
     while( getline(file, temp) )
     {        
         word_6.push_back(temp);
         nol[6]++;
     }
-    file.close();    
+    
+    file.open("words/7.txt");        
+    while( getline(file, temp) )
+    {        
+        word_7.push_back(temp);
+        nol[7]++;
+    }
+    file.close();
+    
+    file.open("words/8.txt");        
+    while( getline(file, temp) )
+    {        
+        word_8.push_back(temp);
+        nol[8]++;
+    }
+    file.close();
+    
+    file.open("words/9.txt");        
+    while( getline(file, temp) )
+    {        
+        word_9.push_back(temp);
+        nol[9]++;
+    }
+    file.close();
+    
+    file.open("words/10.txt");        
+    while( getline(file, temp) )
+    {        
+        word_10.push_back(temp);
+        nol[10]++;
+    }
+    file.close();
+    
+    file.open("words/11.txt");        
+    while( getline(file, temp) )
+    {        
+        word_11.push_back(temp);
+        nol[11]++;
+    }
+    file.close();
+    
+    file.open("words/12.txt");        
+    while( getline(file, temp) )
+    {        
+        word_12.push_back(temp);
+        nol[12]++;
+    }
+    file.close();
+    
+    file.open("words/13.txt");        
+    while( getline(file, temp) )
+    {        
+        word_13.push_back(temp);
+        nol[13]++;
+    }
+    file.close();
+    
+    file.open("words/14.txt");        
+    while( getline(file, temp) )
+    {        
+        word_14.push_back(temp);
+        nol[14]++;
+    }
+    file.close();
+    
+    file.open("words/15.txt");        
+    while( getline(file, temp) )
+    {        
+        word_15.push_back(temp);
+        nol[15]++;
+    }
+    file.close();
+    
 }
 
-void Cpuzzle::def_puzzle_1()
+void Cpuzzle::def_puzzle()
 {
-    xPosition.push_back(0);
-    xPosition.push_back(0);
-    xPosition.push_back(1);
-    xPosition.push_back(3);
-    xPosition.push_back(5);
-    xPosition.push_back(3);
-    xPosition.push_back(5);
-    xPosition.push_back(2);    
-    
+    xPosition.push_back(0);    
     yPosition.push_back(0);
-    yPosition.push_back(2);
-    yPosition.push_back(0);
-    yPosition.push_back(0);
-    yPosition.push_back(0);
-    yPosition.push_back(4);
-    yPosition.push_back(4);
-    yPosition.push_back(5);
-    
-    puzzle_word.push_back(6);
-    puzzle_word.push_back(6);
-    puzzle_word.push_back(5);
-    puzzle_word.push_back(3);
-    puzzle_word.push_back(3);
-    puzzle_word.push_back(2);
-    puzzle_word.push_back(2);
-    puzzle_word.push_back(4);
-    
-    order.push_back(1);
-    order.push_back(0);                
     order.push_back(0);
-    order.push_back(1);
-    order.push_back(1);    
-    order.push_back(1);
-    order.push_back(1);    
-    order.push_back(0);
-}
-
-void Cpuzzle::def_puzzle_2()
-{
-    xPosition.push_back(0);
-    xPosition.push_back(0);
-    xPosition.push_back(0);
-    xPosition.push_back(0);
-    xPosition.push_back(2);
-    xPosition.push_back(4);
-    xPosition.push_back(3);
-    xPosition.push_back(4);    
+    lenght.push_back(15);
     
+    xPosition.push_back(0);    
     yPosition.push_back(0);
-    yPosition.push_back(1);
-    yPosition.push_back(3);
-    yPosition.push_back(5);
-    yPosition.push_back(0);
-    yPosition.push_back(0);
-    yPosition.push_back(3);
-    yPosition.push_back(0);
-    
-    puzzle_word.push_back(6);
-    puzzle_word.push_back(5);
-    puzzle_word.push_back(6);
-    puzzle_word.push_back(6);
-    puzzle_word.push_back(4);
-    puzzle_word.push_back(4);
-    puzzle_word.push_back(3);
-    puzzle_word.push_back(2);
-    
     order.push_back(1);
-    order.push_back(0);                
-    order.push_back(0);
-    order.push_back(0);
-    order.push_back(1);    
-    order.push_back(1);
-    order.push_back(1);    
-    order.push_back(0);
+    lenght.push_back(15);        
 }
 
 void Cpuzzle::initPuzzle()
@@ -199,44 +179,80 @@ void Cpuzzle::initPuzzle()
     bool Match=true;
     srand(time(0));
     
-    for(unsigned int i=0;i<puzzle_word.size();i++)
+    for(unsigned int i=0;i<lenght.size();i++)
     {   
         
         cnt=0;
         
-        while( cnt < ( nol[ puzzle_word[i] ]*2 ) )
+        while( cnt < ( nol[ lenght[i] ]*2 ) )
         {            
             Match=true;
-            switch(puzzle_word[i])
+            switch(lenght[i])
             {
                 case 2:
-                    n = rand() % (nol[ puzzle_word[i] ]-1);
+                    n = rand() % (nol[ lenght[i] ]-1);
                     temp = word_2[ n ]; 
                     break;
                 case 3: 
-                    n = rand() % (nol[ puzzle_word[i] ]-1);
+                    n = rand() % (nol[ lenght[i] ]-1);
                     temp = word_3[ n ];  
                     break;
                 case 4: 
-                    n = rand() % (nol[ puzzle_word[i] ]-1);
+                    n = rand() % (nol[ lenght[i] ]-1);
                     temp = word_4[ n ]; 
                     break;
                 case 5: 
-                    n = rand() % (nol[ puzzle_word[i] ]-1);
+                    n = rand() % (nol[ lenght[i] ]-1);
                     temp = word_5[ n ];  
                     break;
                 case 6:
-                    n= rand() % (nol[ puzzle_word[i] ] -1);
+                    n= rand() % (nol[ lenght[i] ] -1);
                     temp = word_6[ n ];  
+                    break;
+                case 7:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_7[ n ];  
+                    break;
+                case 8:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_8[ n ];  
+                    break;
+                case 9:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_9[ n ];  
+                    break;
+                case 10:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_10[ n ];  
+                    break;
+                case 11:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_11[ n ];  
+                    break;
+                case 12:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_12[ n ];  
+                    break;
+                case 13:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_13[ n ];  
+                    break;
+                case 14:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_14[ n ];  
+                    break;
+                case 15:
+                    n= rand() % (nol[ lenght[i] ] -1);
+                    temp = word_15[ n ];  
                     break;
             }//end switch                   
         
             x = xPosition[i];
             y = yPosition[i];
         
-            for(int j=0;j<puzzle_word[i];j++)
+            for(int j=0;j<lenght[i];j++)
             {                                                                                        
-                if(elements_6[x].at(y) !='#' && elements_6[x].at(y) != temp.at(j) )
+                if(elements[x].at(y) !='#' && elements[x].at(y) != temp.at(j) )
                     {                           
                         Match=false;
                     }//end if
@@ -248,11 +264,11 @@ void Cpuzzle::initPuzzle()
             {
                 x = xPosition[i];
                 y = yPosition[i];
-                cnt=nol[ puzzle_word[i] ]*2+1;                
+                cnt=nol[ lenght[i] ]*2+1;                
                 
                 for(unsigned int j=0;j<temp.length();j++)
                 {                    
-                    elements_6[x].at(y) = temp.at(j);
+                    elements[x].at(y) = temp.at(j);
                     x+=!order[i];
                     y+=order[i];                    
                 }//end for
@@ -262,31 +278,18 @@ void Cpuzzle::initPuzzle()
         if(!Match)
         {        
             i=-1;               
-            clear(6);
+            clear();
         }
     }//end for
         
 }
 
-void Cpuzzle::display(int size)
+void Cpuzzle::display()
 {
-    switch(size)
+    for(int i=0;i<15;i++)
     {
-        case 3: 
-            for(int i=0;i<3;i++)
-                    cout<<elements_3[i]<<endl; 
-            break;
-        case 4: 
-            for(int i=0;i<4;i++)
-                cout<<elements_4[i]<<endl; 
-            break;
-        case 5: 
-            for(int i=0;i<5;i++)
-                cout<<elements_5[i]<<endl; 
-            break;
-        case 6: 
-            for(int i=0;i<6;i++)
-                cout<<elements_6[i]<<endl; 
-            break;
+        for(int j=0;j<15;j++)
+            cout<<elements[i][j]<<" "; 
+        cout<<endl;
     }
 }
